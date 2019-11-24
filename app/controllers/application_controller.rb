8178337f9e14add4c90ce1273e9e1a757982f6c8
Zach  Smith <zdsmith001@gmail.com>
2019-11-24 23:31:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,14 @@ class App < Sinatra::Base
     post "/teams" do
       @team_name = params[:team][:name]
       @team_motto = params[:team][:motto]
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
+      params[:team][:members].each do |hero|
+        @hero_name << hero[:name]
+        @hero_power << hero[:power]
+        @hero_bio << hero[:bio]
+      end
       erb :team
     end
 
